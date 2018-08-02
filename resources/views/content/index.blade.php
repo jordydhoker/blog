bce7fe85e3a1c2e7ab3bf99bb70f776e6e894302
Jordy DHoker <jordydhoker@gmail.com>
2018-08-02 02:02:27 +0200

    Header CSS

--- resources/views/content/index.blade.php
+++ resources/views/content/index.blade.php
@@ -2,17 +2,67 @@
 
 @section('content')
         <main class="masonry">
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
-            <article>test</article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x1000">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x200">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/600x110">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x1000">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/600x110">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x1000">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x200">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x1000">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x1000">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/600x110">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x1000">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/600x110">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x1000">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x200">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+            <article><h3>testmaatie</h3>
+                <img src="http://via.placeholder.com/500x200">
+                <p>testevougnwpkercgbahbfoxaoscgnaxrigxlcjhsbakhxbkscdbnsxbcjvbcdmcgxcvncsdvoguwcgaiergxmscbgosdiuhvg</p>
+            </article>
+
+
 </main>
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <a class="logo" href=""><img src="http://via.placeholder.com/100x100"></a>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Profile</a>
+        <a href="">Admin</a>
+    </nav>
+</header>
